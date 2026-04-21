745e02e7f2b5ba626413b60392c257ff4dd677b0
Aasrith <asatorchbearers@gmail.com>
2026-04-21 15:04:44 +0530

    Coded UC1: Display Empty 3x3 Tic-Tac-Toe Board

---
+++ TicTacToe.java
@@ -0,0 +1,36 @@
+/**
+ * TicTacToeApp UC1 - Display Empty Tic-Tac-Toe Board
+ *
+ * This class demonstrates the initialization and display of a 3x3
+ * Tic-Tac-Toe board using a 2D array and nested loops.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        // 1. Create a 3x3 character array to represent the game board
+        char[][] board = new char[3][3];
+
+        // 2. Initialize all cells with the '-' placeholder using nested loops
+        for (int i = 0; i < 3; i++) {         // Outer loop for rows
+            for (int j = 0; j < 3; j++) {     // Inner loop for columns
+                board[i][j] = '-';
+            }
+        }
+
+        // 3. Print the board clearly to the console
+        System.out.println("Welcome to Tic-Tac-Toe!");
+        System.out.println("Current Board:");
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                // Print each cell followed by a space for readability
+                System.out.print(board[i][j] + " ");
+            }
+            // Move to the next line after completing a row
+            System.out.println();
+        }
+    }
+}
